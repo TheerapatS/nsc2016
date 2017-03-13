08d30cb7ba3d8c4e9681d11103d30e8ba3685a24
omhsin007 <makoto.diva@gmail.com>
2017-03-13 22:18:04 +0700

    skel with cut end

--- msertest.py
+++ msertest.py
@@ -3,6 +3,91 @@ import scipy.ndimage.morphology as m
 import cv2
 
 
+def check_near(point, skel, x, y):
+    if point[0] == 0 and point[1] == 0:
+        if skel[point[0] + 1][point[1]] == 1: return 1, point[0] + 1, point[1]
+        if skel[point[0]][point[1] + 1] == 1: return 1, point[0], point[1] + 1
+        if skel[point[0] + 1][point[1] + 1] == 1: return 1, point[0] + 1, point[1] + 1
+    else:
+        if point[0] == 0 and point[1] == y - 1:
+            if skel[point[0]][point[1] - 1] == 1: return 1, point[0], point[1] - 1
+            if skel[point[0] + 1][point[1] - 1] == 1: return 1, point[0] + 1, point[1] - 1
+            if skel[point[0] + 1][point[1]] == 1: return 1, point[0] + 1, point[1]
+        else:
+            if point[0] == x - 1 and point[1] == 0:
+                if skel[point[0] - 1][point[1]] == 1: return 1, point[0] - 1, point[1]
+                if skel[point[0] - 1][point[1] + 1] == 1: return 1, point[0] - 1, point[1] + 1
+                if skel[point[0]][point[1] + 1] == 1: return 1, point[0], point[1] + 1
+            else:
+                if point[0] == x - 1 and point[1] == y - 1:
+                    if skel[point[0] - 1][point[1] - 1] == 1: return 1, point[0] - 1, point[1] - 1
+                    if skel[point[0]][point[1] - 1] == 1: return 1, point[0], point[1] - 1
+                    if skel[point[0] - 1][point[1]] == 1: return 1, point[0] - 1, point[1]
+                else:
+                    if point[0] == 0 and 0 <= point[1] < y:
+                        if skel[point[0]][point[1] - 1] == 1: return 1, point[0], point[1] - 1
+                        if skel[point[0] + 1][point[1] - 1] == 1: return 1, point[0] + 1, point[1] - 1
+                        if skel[point[0] + 1][point[1]] == 1: return 1, point[0] + 1, point[1]
+                        if skel[point[0]][point[1] + 1] == 1: return 1, point[0], point[1] + 1
+                        if skel[point[0] + 1][point[1] + 1] == 1: return 1, point[0] + 1, point[1] + 1
+                    else:
+                        if point[0] == x - 1 and 0 <= point[1] < y:
+                            if skel[point[0] - 1][point[1] - 1] == 1: return 1, point[0] - 1, point[1] - 1
+                            if skel[point[0]][point[1] - 1] == 1: return 1, point[0], point[1] - 1
+                            if skel[point[0] - 1][point[1]] == 1: return 1, point[0] - 1, point[1]
+                            if skel[point[0] - 1][point[1] + 1] == 1: return 1, point[0] - 1, point[1] + 1
+                            if skel[point[0]][point[1] + 1] == 1: return 1, point[0], point[1] + 1
+                        else:
+                            if 0 <= point[0] < x and point[1] == 0:
+                                if skel[point[0] - 1][point[1]] == 1: return 1, point[0] - 1, point[1]
+                                if skel[point[0] + 1][point[1]] == 1: return 1, point[0] + 1, point[1]
+                                if skel[point[0] - 1][point[1] + 1] == 1: return 1, point[0] - 1, point[1] + 1
+                                if skel[point[0]][point[1] + 1] == 1: return 1, point[0], point[1] + 1
+                                if skel[point[0] + 1][point[1] + 1] == 1: return 1, point[0] + 1, point[1] + 1
+                            else:
+                                if 0 <= point[0] < x and point[1] == y - 1:
+                                    if skel[point[0] - 1][point[1] - 1] == 1: return 1, point[0] - 1, point[1] - 1
+                                    if skel[point[0]][point[1] - 1] == 1: return 1, point[0], point[1] - 1
+                                    if skel[point[0] + 1][point[1] - 1] == 1: return 1, point[0] + 1, point[1] - 1
+                                    if skel[point[0] - 1][point[1]] == 1: return 1, point[0] - 1, point[1]
+                                    if skel[point[0] + 1][point[1]] == 1: return 1, point[0] + 1, point[1]
+                                else:
+                                    if 0 <= point[0] < x and 0 <= point[1] < y:
+                                        if skel[point[0] - 1][point[1] - 1] == 1: return 1, point[0] - 1, point[1] - 1
+                                        if skel[point[0]][point[1] - 1] == 1: return 1, point[0], point[1] - 1
+                                        if skel[point[0] + 1][point[1] - 1] == 1: return 1, point[0] + 1, point[1] - 1
+                                        if skel[point[0] - 1][point[1]] == 1: return 1, point[0] - 1, point[1]
+                                        if skel[point[0] + 1][point[1]] == 1: return 1, point[0] + 1, point[1]
+                                        if skel[point[0] - 1][point[1] + 1] == 1: return 1, point[0] - 1, point[1] + 1
+                                        if skel[point[0]][point[1] + 1] == 1: return 1, point[0], point[1] + 1
+                                        if skel[point[0] + 1][point[1] + 1] == 1: return 1, point[0] + 1, point[1] + 1
+    return 0, 0, 0
+
+
+def br_near(point, skel):
+    t = 0
+    if skel[point[0] - 1][point[1] - 1] == 1: t += 1
+    if skel[point[0]][point[1] - 1] == 1: t += 1
+    if skel[point[0] + 1][point[1] - 1] == 1: t += 1
+    if skel[point[0] - 1][point[1]] == 1: t += 1
+    if skel[point[0] + 1][point[1]] == 1: t += 1
+    if skel[point[0] - 1][point[1] + 1] == 1: t += 1
+    if skel[point[0]][point[1] + 1] == 1: t += 1
+    if skel[point[0] + 1][point[1] + 1] == 1: t += 1
+    return t
+
+
+def del_end(point, br_map, skel):
+    while br_map[point[0]][point[1]] != 255:
+        skel[point[0]][point[1]] = 0
+        t, i, j = check_near(point, skel, skel.shape[0], skel.shape[1])
+        if t == 0:
+            break
+        else:
+            point = [i, j]
+    return skel
+
+
 def skeletonize(img):
     h1 = np.array([[0, 0, 0], [0, 1, 0], [1, 1, 1]])
     m1 = np.array([[1, 1, 1], [0, 0, 0], [0, 0, 0]])
@@ -26,17 +111,17 @@ def skeletonize(img):
     return img
 
 
-def find_skel(img, im, c):
+def find_skel(img, im, c, n_pic):
     x = im.shape
     element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
     img = cv2.dilate(img, element, iterations=3)
     skel = skeletonize(img)
     # cv2.imshow(str(c), skel.astype(np.uint8) * 255)
-    # cv2.imwrite("skel_" + str(c) + ".jpg", skel.astype(np.uint8) * 255)
+    cv2.imwrite("F:/NSC/Sample/" + str(n_pic) + "skel.jpg", skel.astype(np.uint8) * 255)
     c += 1
     tempimg = np.zeros((int(x[0] + 2), int(x[1] + 2)), np.uint8)
-    for i in range(1, x[0]):
-        for j in range(1, x[1]):
+    for i in range(1, x[0] + 1):
+        for j in range(1, x[1] + 1):
             if skel[i - 1][j - 1]:
                 tempimg[i][j] = 1
             else:
@@ -45,8 +130,8 @@ def find_skel(img, im, c):
     br_list = []
     end_map = np.zeros((int(x[0]), int(x[1])), np.uint8)
     end_list = []
-    for i in range(1, x[0]):
-        for j in range(1, x[1]):
+    for i in range(1, x[0] + 1):
+        for j in range(1, x[1] + 1):
             if tempimg[i][j] == 1:
                 br = tempimg[i - 1][j - 1] + tempimg[i - 1][j] + tempimg[i - 1][j + 1] + tempimg[i][j - 1] + tempimg[i][
                     j + 1] + tempimg[i + 1][j - 1] + tempimg[i + 1][j] + tempimg[i + 1][j + 1]
@@ -56,6 +141,7 @@ def find_skel(img, im, c):
                 if br == 1:
                     end_map[i - 1][j - 1] = 255
                     end_list.append([i - 1, j - 1])
+    cv2.imwrite("F:/NSC/Sample/" + str(n_pic) + "end01.jpg", end_map.astype(np.uint8))
     return c, br_map, br_list, end_map, end_list, skel
 
 
@@ -82,8 +168,8 @@ def get_txt(path):
     return im
 
 
-def get_skel(path):
-    countwindows = 0;
+def get_skel(path, n_pic):
+    countwindows = 0
     img = cv2.imread(path)
     shape = img.shape
     fixed_length = 800.00
@@ -104,7 +190,7 @@ def get_skel(path):
         im = np.zeros((int(x[0]), int(x[1])), np.uint8)
         for i in range(0, len(region)):
             im[region[i][1]][region[i][0]] = 255
-        temp = im_fill(im);
+        temp = im_fill(im)
         im_filled.append(temp.copy())
         area2.append(cv2.countNonZero(temp))
         contour2, hier2 = cv2.findContours(temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -159,15 +245,34 @@ def get_skel(path):
         im_filled[ind] = cv2.erode(im_filled[ind], element, iterations=1)
         im_filled[ind] = cv2.dilate(im_filled[ind], element, iterations=1)
 
-        # cv2.imshow(str(countwindows + 300), im_filled[ind])
-        # cv2.imwrite(str(countwindows + 200)+".jpg", im_filled[ind])
-        __1, __2, __3, end_map, end_list, skel = find_skel(im_filled[ind], img, countwindows)
-        # cv2.imwrite(str(countwindows + 400)+".jpg", skel)
+        cv2.imwrite("F:/NSC/Sample/" + str(n_pic) + "_road mark.jpg", im_filled[ind])
+        __1, br_map, br_list, end_map, end_list, skel = find_skel(im_filled[ind], img, countwindows, n_pic)
+
+        # print end_list
+        # print br_list
+        # print skel.shape
+        # print end_map.shape
+        cv2.imwrite("F:/NSC/Sample/" + str(n_pic) + "br01.jpg", br_map.astype(np.uint8))
+        # print end_list
+        for i in end_list:
+            skel = del_end(i, br_map, skel)
+
+        tempimg = np.zeros((int(x[0] + 2), int(x[1] + 2)), np.uint8)
+        for i in range(1, x[0] + 1):
+            for j in range(1, x[1] + 1):
+                if skel[i - 1][j - 1]:
+                    tempimg[i][j] = 1
+                else:
+                    tempimg[i][j] = 0
+
+        cv2.imwrite("F:/NSC/Sample/" + str(n_pic) + "_skel_cut.jpg", skel.astype(np.uint8) * 255)
         return img, skel, ratio_resize
-    return 0, 0
+
+    return 0, 0, 0
 
 
 if __name__ == '__main__':
-    img, skel, ratio = get_skel("F:/NSC/Sample/68.jpg")
-
+    # for n_pic in range(20,65):
+    #      img, skel, ratio = get_skel("F:/NSC/Sample/"+ str(n_pic)+".jpg",n_pic)
+    img, skel, ratio = get_skel("F:/NSC/Sample/" + str(61) + ".jpg", 61)
     cv2.waitKey()
